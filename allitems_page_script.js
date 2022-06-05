ce69b6e10efea7bca6971e2bded6f325b450d3a0
adityoqrren <aji.adityo8@gmail.com>
2022-06-05 23:26:53 +0700

    filter sidebar complete

--- allitems_page_script.js
+++ allitems_page_script.js
@@ -1,24 +1,58 @@
 // Tab JS
 
-tabButtons = document.querySelectorAll('.tab-button');
-tabContents = document.querySelectorAll('.tab-contents-box');
+// tabButtons = document.querySelectorAll('.tab-button');
+// tabContents = document.querySelectorAll('.tab-contents-box');
 
-tabButtons.forEach(tabButton => {
-    tabButton.addEventListener('click', function handleClick(event){
+// tabButtons.forEach(tabButton => {
+//     tabButton.addEventListener('click', function handleClick(event){
        
-        for (i=0; i<tabContents.length; i++){
-             //kita sembunyikan semua tab-contents-box
-            tabContents[i].classList.remove('active');
-            //kita hapus dulu kelas active di semua tab-button
-            tabButtons[i].classList.remove('active');  
+//         for (i=0; i<tabContents.length; i++){
+//              //kita sembunyikan semua tab-contents-box
+//             tabContents[i].classList.remove('active');
+//             //kita hapus dulu kelas active di semua tab-button
+//             tabButtons[i].classList.remove('active');  
+//         }
+//         //kita tambahin kelas active di button yang sekarang terklik
+//         event.target.classList.add('active');
+//         //kita tampilkan tab-content sesuai yang diminta 
+//         data = event.target.dataset.content;
+//         contentToOpen = document.querySelector('#tab-box-'+data);
+//         contentToOpen.classList.add("active");
+//     });
+// });
+
+// document.getElementById("defaultOpen").click();
+
+// Left Filter Open-Close
+
+allButtonsOpen = document.querySelectorAll('.btn-expand-collapse');
+// allListFilter = document.querySelectorAll('.filter-left ul');
+
+allButtonsOpen.forEach(buttonOpen => {
+    buttonOpen.addEventListener('click', function handleClick(event){
+        if(event.target.textContent=="+"){
+            event.target.textContent = "-";
+        }else{
+            event.target.textContent = "+";
         }
-        //kita tambahin kelas active di button yang sekarang terklik
-        event.target.classList.add('active');
-        //kita tampilkan tab-content sesuai yang diminta 
-        data = event.target.dataset.content;
-        contentToOpen = document.querySelector('#tab-box-'+data);
-        contentToOpen.classList.add("active");
+        parentThisNode = event.target.parentElement.parentElement;
+        console.log(parentThisNode);
+        listThisNode = parentThisNode.querySelector("ul");
+        listThisNode.classList.toggle('filter-hide');
     });
+})
+
+// open default left filter
+document.getElementById("btn-expand-default").click();
+
+// slide filter right (filter for minimize or mobile)
+const filterRightButton = document.querySelector(".filter-button");
+const filterRight = document.querySelector(".filter-right");
+filterRightButton.addEventListener('click',function(){
+    filterRight.classList.toggle('slide');
 });
 
-document.getElementById("defaultOpen").click();
+const filterClose = document.querySelector(".filter-close");
+filterClose.addEventListener('click',function(){
+    filterRight.classList.remove('slide');
+});
